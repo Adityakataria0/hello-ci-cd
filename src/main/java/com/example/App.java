0b8714992efeb8f3642362ace9a8d68708243374
Adityakataria0 <https://github.com/Adityakataria0>
2025-12-10 04:50:36 +0000

    Test: updated message to trigger jenkins build

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-       System.out.println("Hello from my first DevOps pipeline!");
+       System.out.println("Hello from jenkins this is a automated build test");
     }
 }
